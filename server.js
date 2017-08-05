1ef7b2cb5d5baf81cf33dce71d13ecaf709a18c3
jobizzz <jobisjames10@gmail.com>
2017-08-05 17:13:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/artice-one', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send("Article one");
 });
 
-app.get('/artice-two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send("Article two");
 });
 
-app.get('/artice-three', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send("Article three");
 });
 
