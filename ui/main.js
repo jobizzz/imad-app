c705806845172a45126ab0d1f443bac6dc088fea
jobizzz <jobisjames10@gmail.com>
2017-08-06 17:35:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,14 @@
-console.log('Loaded!');
 
 //changing the conntent of page
-var element=document.getElementById("main-text");
-element.innerHTML="New Value";
-
-//TO move the image on click
-var img=document.getElementById("madi");
-var marginLeft=0;
-var moveRight=function(){
-  marginLeft+=1;
-  img.style.marginLeft=marginLeft + "px";
-};
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+var button=document.getElementById("counter");
+var  counter=0;
+button.onclick = function(){
+    //Make a request to the counter end point
+    
+    //Capture the response and store it in a variable
+    
+    //Render the variable in the correct span
+    counter++;
+    var span=document.getElementById("count");
+    span.innerHTML=counter.toString();
 };
