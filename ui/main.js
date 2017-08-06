c3235528e57386aefd6b6fc1a4953f7e62b803cd
jobizzz <jobisjames10@gmail.com>
2017-08-06 14:19:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //changing the conntent of page
 var element=document.getElementById("main-text");
 element.innerHTML="New Value";
+
+//TO move the image on click
+var img=document.getElementById("madi");
+img.onclick=function(){
+    img.style.marginLeft="100 px";
+};
