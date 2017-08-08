b589f1879a22049cc99771f310162d090fcc9669
jobizzz <jobisjames10@gmail.com>
2017-08-08 15:52:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,6 +30,6 @@ submit.onclick= function(){
   var list=" ";
   for(var i=0;i<names.length;i++)
     list+="<li>"+names[i]+"</li";
-  var ul=document.getElementById("namelist")
+  var ul=document.getElementById("namelist");
   ul.innnerHTML=list;
 };
