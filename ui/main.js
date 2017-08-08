0d9e06f3b8fda9c57fd6b69a307e47baec8d9ce3
jobizzz <jobisjames10@gmail.com>
2017-08-08 15:56:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ var submit=document.getElementById("submit_btn");
 submit.onclick= function(){
 
   var names=["names1","names2","names3","names4"];  
-  var list=" ";
+  var list='';
   for(var i=0;i<names.length;i++)
     list+='<li>'+names[i]+'</li';
   var ul=document.getElementById("namelist");
