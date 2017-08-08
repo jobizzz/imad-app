a3f79c2f85743c1a22925e73fcb19426868e36a0
jobizzz <jobisjames10@gmail.com>
2017-08-08 17:14:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,10 +26,29 @@ var name=nameInput.value;
 var submit=document.getElementById("submit_btn");
 submit.onclick= function(){
 
-  var names=['names1','names2','names3','names4'];  
-  var list='';
-  for(var i=0;i<names.length;i++)
-    list+='<li>'+names[i]+'</li>';
-  var ul=document.getElementById("namelist");
-  ul.innerHTML=list;
+     var request=new XMLHttpRequest();
+    //Capture the response and store it in a variable
+    request.onreadystatechange=function(){
+      if(request.readyState===XMLHttpRequest.DONE) 
+      {
+          if(request.status===200)
+          {
+            var counter=request.responseText;
+            var span=document.getElementById("count");
+            span.innerHTML=counter.toString();
+          
+      
+        
+          var names=request.responseText;
+          names=JSON.parse(names);
+          var list='';
+          for(var i=0;i<names.length;i++)
+            list+='<li>'+names[i]+'</li>';
+          var ul=document.getElementById("namelist");
+          ul.innerHTML=list;
+          }
+      }
+    };
+    request.open("GET","http://jobisjames10.imad.hasura-app.io/submit?name="+name,true);
+    request.send(null);
 };
