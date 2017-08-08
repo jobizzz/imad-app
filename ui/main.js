c1d005c215417366b1149c89e09515a55a5a5775
jobizzz <jobisjames10@gmail.com>
2017-08-08 19:21:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,30 +26,26 @@ var submit=document.getElementById("submit_btn");
 submit.onclick= function(){
 
      var request=new XMLHttpRequest();
-     var nameInput=document.getElementById("name");
-     var name=nameInput.value;
+
     //Capture the response and store it in a variable
     request.onreadystatechange=function(){
       if(request.readyState===XMLHttpRequest.DONE) 
       {
           if(request.status===200)
           {
-            var counter=request.responseText;
-            var span=document.getElementById("count");
-            span.innerHTML=counter.toString();
-          
-      
-        
-          var names=request.responseText;
-          names=JSON.parse(names);
-          var list='';
-          for(var i=0;i<names.length;i++)
-            list+='<li>'+names[i]+'</li>';
-          var ul=document.getElementById("namelist");
-          ul.innerHTML=list;
+             
+              var names=request.responseText;
+              names=JSON.parse(names);
+              var list='';
+              for(var i=0;i<names.length;i++)
+                list+='<li>'+names[i]+'</li>';
+              var ul=document.getElementById("namelist");
+              ul.innerHTML=list;
           }
       }
     };
+    var nameInput=document.getElementById("name");
+     var name=nameInput.value;
     request.open("GET","http://jobisjames10.imad.hasura-app.io/submit?name="+name,true);
     request.send(null);
 };
