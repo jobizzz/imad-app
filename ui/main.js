fb7ad5ac1366cedc257901f7d8a85566684b8f88
jobizzz <jobisjames10@gmail.com>
2017-08-23 17:16:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,9 +27,36 @@ submit.onclick= function(){
     };
     var username=document.getElementById("username").value;
      var password=document.getElementById("password").value;
-    console.log(username);
-    console.log(password);
     request.open("POST","http://jobisjames10.imad.hasura-app.io/login",true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
+};
+
+//Register
+
+var reg=document.getElementById("submit_reg");
+reg.onclick= function(){
+
+     var request=new XMLHttpRequest();
+
+    //Capture the response and store it in a variable
+    request.onreadystatechange=function(){
+      if(request.readyState===XMLHttpRequest.DONE) 
+      {
+          if(request.status===500) {
+             alert("something went wrong in server");
+          }
+          else {
+             
+             alert("REGISTERED SUCCESSFULLY");
+          }
+         
+      }
+      
+    };
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    request.open("POST","http://jobisjames10.imad.hasura-app.io/create-user",true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:username,password:password}));
 };
