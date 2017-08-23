309e2ac420202762b11062f44f50a430394584c8
jobizzz <jobisjames10@gmail.com>
2017-08-23 16:48:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -171,6 +171,11 @@ app.get('/check-login',function(req,res){
    
 });
 
+app.get('/logout',function(req,res){
+   delete req.session.auth;
+   res.send("Logged Out");
+});
+
 var pool=new Pool(config);
 app.get('/test-db', function (req, res) {
   //Make a select request
