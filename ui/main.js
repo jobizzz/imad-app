3bd43cea348796c76a913e22c09ed6ce39c15ca5
jobizzz <jobisjames10@gmail.com>
2017-08-23 15:03:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,4 @@
-
-//changing the conntent of page
-var button=document.getElementById("counter");
-var  counter=0;
-button.onclick = function(){
-    //Create a request object
-    var request=new XMLHttpRequest();
-    //Capture the response and store it in a variable
-    request.onreadystatechange=function(){
-      if(request.readyState===XMLHttpRequest.DONE) 
-      {
-          if(request.status===200)
-          {
-            var counter=request.responseText;
-            var span=document.getElementById("count");
-            span.innerHTML=counter.toString();
-          }
-      }
-    };
-    //Make a request
-    request.open("GET","http://jobisjames10.imad.hasura-app.io/counter",true);
-    request.send(null);
-};
+//submit username & password
 
 var submit=document.getElementById("submit_btn");
 submit.onclick= function(){
@@ -33,22 +11,28 @@ submit.onclick= function(){
       {
           if(request.status===200)
           {
-             
-              var names=request.responseText;
-              names=JSON.parse(names);
-              var list='';
-              for(var i=0;i<names.length;i++)
-                list+='<li>'+names[i]+'</li>';
-              var ul=document.getElementById("namelist");
-              ul.innerHTML=list;
+             console.log("User logged in ");
+             alert("LOGGED IN SUCCESSFULLY");
+          }
+          else if(request.status===403)
+          {
+             alert("Invalid username/password");
+          }
+          else if(request.status===500)
+         {
+             alert("something went wrong in server");
           }
       }
       
     };
-    var nameInput=document.getElementById("name");
-     var name=nameInput.value;
-    request.open("GET","http://jobisjames10.imad.hasura-app.io/submit?name="+name,true);
-    request.send(null);
+    var username=document.getElementById("username").value;
+     var password=document.getElementById("password").value;
+    console.log("username");
+    console.log("password");
+    request.open("POST","http://jobisjames10.imad.hasura-app.io/login",true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:"username",password:"password"}));
 };
+
 
 
