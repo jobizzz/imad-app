5a1bf0fe31d8cdda7653db9dbec1dda82f855677
jobizzz <jobisjames10@gmail.com>
2017-08-23 18:49:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@ submit.onclick= function(){
           {
              console.log("User logged in ");
              alert("LOGGED IN SUCCESSFULLY");
-             login.innerHTML="LOGOUT";
+             login.innerHTML="<p>${username}</p>";
           }
           else if(request.status===403)
           {
