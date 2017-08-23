ff2c6448e6302f1344b431664bc53d1b7dad77f1
jobizzz <jobisjames10@gmail.com>
2017-08-23 19:12:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -97,11 +97,11 @@ logout.onclick= function(){
       }
       
     };
-    var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
-    request.open("POST","http://jobisjames10.imad.hasura-app.io/logout",true);
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username:username,password:password}));
+  //  var username=document.getElementById("username").value;
+   // var password=document.getElementById("password").value;
+    request.open("GET","http://jobisjames10.imad.hasura-app.io/logout",true);
+   // request.setRequestHeader('Content-Type','application/json');
+  //  request.send(JSON.stringify({username:username,password:password}));
 };
 
 
